36992452c996325ce86487c80bbef5d82264f9b9
Hyogeun2 <35337131+Hyogeun2@users.noreply.github.com>
2021-05-06 15:05:39 +0900

    JS navbar click

--- main.js
+++ main.js
@@ -13,3 +13,17 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 }); //스크롤이 될 때 마다 실행시켜줘
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  } else {
+    console.log(link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: "smooth" });
+  }
+});
